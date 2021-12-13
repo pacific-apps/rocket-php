e0e295d4400fe8095a26e5bf06aad529b4db5f61
rterrado <terrado.ken@gmail.com>
2021-12-13 14:32:26 +0800

    public-repo-lib

--- lib/rocket.php
+++ lib/rocket.php
@@ -1,5 +1,5 @@
 <?php
 
-const ROCKET_HOST = 'http://localhost:3000/';
+const ROCKET_HOST = 'https://brave-albattani-b01a39.netlify.app/';
 const CORE = 'core';
 const ERROR_MODULE_DO_NOT_EXIST = 'Module do not exist';
